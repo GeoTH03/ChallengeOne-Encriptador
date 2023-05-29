549a04a2938a38db1ce75bd78735e51ce1424535
Geovanny Torres Hernandez <rescuenta20@gmail.com>
2023-05-29 15:11:50 -0600

    agregando funciones al script

--- encrypter.js
+++ encrypter.js
@@ -1,18 +1,50 @@
 const reglaEncriptado = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];//matriz, continene las reglas para el encriptado
+const cajaEncriptado = document.querySelector(".caja2");
+const mensaje1 = document.querySelector(".aviso1");
+const mensaje2= document.querySelector(".aviso2");
 const inputText = document.querySelector(".inputText");
 const outputText = document.querySelector(".outPutText");
 let btnEncriptar= document.querySelector("#encripta");
 let btnDesencriptar=document.querySelector("#desencripta")
 let btnCopiar=document.querySelector("#copiar"); /*variables necesarias para enlazar al html*/
+const tamanio= document.querySelector("body");
+const mensajeCopiado = document.querySelector("#mensajeInvisible");
+
+
 function iniciaEncriptado(){
         let resultadoEncriptacion = encriptar(inputText.value);
         outputText.value = resultadoEncriptacion;
+        if(outputText.value.length>0){
+            cajaEncriptado.style.background="#000000a6";
+            mensaje1.style.visibility = "hidden";
+            mensaje2.style.visibility = "hidden";
+        }
+        else{
+            if(tamanio.clientWidth > 980){
+            cajaEncriptado.style.background="url(imagenes/Muneco.png) no-repeat center 56px /80% #000000a6";
+            mensaje1.style.visibility = "visible";
+            mensaje2.style.visibility = "visible";
+            }
+        }
 }
 function iniciaDesencriptado(){
     let desencriptado = desencriptar(inputText.value);
     outputText.value = desencriptado;
+    if(outputText.value.length>0){
+        cajaEncriptado.style.background="#000000a6";
+        mensaje1.style.visibility = "hidden";
+        mensaje2.style.visibility = "hidden";
+    }
+    else{
+        if(tamanio.clientWidth > 980){
+        cajaEncriptado.style.background="url(imagenes/Muneco.png) no-repeat center 56px /80% #000000a6";
+        mensaje1.style.visibility = "visible";
+        mensaje2.style.visibility = "visible";
+        }
+    }
 }
 function encriptar(texto){
+    texto = texto.toLowerCase();
     for(let i = 0; i<reglaEncriptado.length;i++){
         if(texto.includes(reglaEncriptado[i][0])){
             texto = texto.replaceAll(reglaEncriptado[i][0],reglaEncriptado[i][1]);
@@ -29,9 +61,36 @@ function desencriptar(textoEncriptado){
     return textoEncriptado;
 }
 function copiar(){
-
     outputText.select();
     outputText.setSelectionRange(0, 99999)
     textocopiado = document.execCommand('copy');
     window.getSelection().removeAllRanges();
+    mensajeCopiado.innerText ="¡Texto copiado al portapapeles!";
+    mensajeCopiado.style.background="#00ffa1cf";
+    setInterval(quitaMensaje,1000);
+}
+function quitaMensaje(){
+    mensajeCopiado.innerText="";
+    mensajeCopiado.style.background="none";
+}
+function leeAreaEncriptado(){//función lee lo que hay en el textarea a encriptar
+    if(outputText.value.length > 0 ){//condicion: si el valor es mayor a 0 se ejecuta
+        outputText.value="";//se le asigna al textarea un string vacio ó sin valor
+          if(tamanio.clientWidth > 980){//condición: si el tamaño de pantalla es mayor a 980px
+                cajaEncriptado.style.background= "url(imagenes/Muneco.png) no-repeat center 56px /80% #000000a6";//se le asigna un background con imagen
+                outputText.value = "";
+                mensaje1.style.visibility = "visible";//se hace visible el mensaje
+                mensaje2.style.visibility = "visible";
+            }
+         
+    }
+    else{   
+       
+        if(tamanio.clientWidth > 980){//condición: si el tamaño de pantalla es mayor a 980px
+            cajaEncriptado.style.background= "url(imagenes/Muneco.png) no-repeat center 56px /80% #000000a6";//se le asigna un background con imagen
+            outputText.value = "";
+            mensaje1.style.visibility = "visible";//se hace visible el mensaje
+            mensaje2.style.visibility = "visible";
+        }
+    }
 }
