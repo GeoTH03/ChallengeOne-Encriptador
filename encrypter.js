f429eca88110b776f60488d82345539be623c5f7
Geovanny Torres Hernandez <rescuenta20@gmail.com>
2023-05-30 17:02:49 -0600

    añadiendo comentarios al script

--- encrypter.js
+++ encrypter.js
@@ -1,4 +1,4 @@
-const reglaEncriptado = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];//matriz, continene las reglas para el encriptado
+const reglaEncriptado = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];//Matriz, contiene las reglas para el encriptado
 const cajaEncriptado = document.querySelector(".caja2");
 const mensaje1 = document.querySelector(".aviso1");
 const mensaje2= document.querySelector(".aviso2");
@@ -6,7 +6,7 @@ const inputText = document.querySelector(".inputText");
 const outputText = document.querySelector(".outPutText");
 let btnEncriptar= document.querySelector("#encripta");
 let btnDesencriptar=document.querySelector("#desencripta")
-let btnCopiar=document.querySelector("#copiar"); /*variables necesarias para enlazar al html*/
+let btnCopiar=document.querySelector("#copiar"); /*Variables necesarias para enlazar al html*/
 const tamanio= document.querySelector("body");
 const mensajeCopiado = document.querySelector("#mensajeInvisible");
 
@@ -20,7 +20,7 @@ function iniciaEncriptado(){
             mensaje2.style.visibility = "hidden";
             btnCopiar.style.visibility="visible";            
             cajaEncriptado.style.height ="50%"
-            inputText.focus();
+            outputText.focus();
         }
         else{
             if(tamanio.clientWidth > 980){
@@ -44,6 +44,7 @@ function iniciaDesencriptado(){
         mensaje2.style.visibility = "hidden";
         cajaEncriptado.style.height ="50%"
         btnCopiar.style.visibility="visible";
+        outputText.focus();
     }
     else{
         if(tamanio.clientWidth > 980){
@@ -53,16 +54,16 @@ function iniciaDesencriptado(){
         
         }
     }
-}
+}/*Función que encripta el texto */
 function encriptar(texto){
-    texto = texto.toLowerCase();
+    texto = texto.toLowerCase();/*convierte en minusculas si el texto esta en mayuscula*/
     for(let i = 0; i<reglaEncriptado.length;i++){
-        if(texto.includes(reglaEncriptado[i][0])){
-            texto = texto.replaceAll(reglaEncriptado[i][0],reglaEncriptado[i][1]);
+        if(texto.includes(reglaEncriptado[i][0])){//compara el contenido y si incluye la letra se ejecuta
+            texto = texto.replaceAll(reglaEncriptado[i][0],reglaEncriptado[i][1]);//reemplaza la letra con el elemento numero 1 de la matriz
         }
     }
-    return texto;
-}
+    return texto;//regresa el resultado de la función
+}/*Función que desencripta el texto */
 function desencriptar(textoEncriptado){
     for(let i = 0; i<reglaEncriptado.length; i++){
         if(textoEncriptado.includes(reglaEncriptado[i][1])){
@@ -70,21 +71,21 @@ function desencriptar(textoEncriptado){
         }
     }
     return textoEncriptado;
-}
+}/*Función de copiado */
 function copiar(){
-    outputText.select();
-    outputText.setSelectionRange(0, 99999)
-    textocopiado = document.execCommand('copy');
-    window.getSelection().removeAllRanges();
-    mensajeCopiado.innerText ="¡Texto copiado al portapapeles!";
+    outputText.select();//Selecciona el texto
+    outputText.setSelectionRange(0, 99999)//Establece un rango de la selección
+    let textocopiado = document.execCommand('copy');//Ejecuta el copiado y lo guarda en la variable
+    window.getSelection().removeAllRanges();//Remueve el rango de selección para que no se muestre
+    mensajeCopiado.innerText ="¡Texto copiado al portapapeles!";//Manda el mensaje copiado con éxito
     mensajeCopiado.style.background="#00ffa1cf";
-    setInterval(quitaMensaje,1500);
+    setInterval(quitaMensaje,1500);//Establece el tiempo en el que se muestra el mensaje
 }
-function quitaMensaje(){
+function quitaMensaje(){/*Elimina el mensaje de copiado */
     mensajeCopiado.innerText="";
     mensajeCopiado.style.background="none";
 }
-function leeAreaEncriptado(){//función lee lo que hay en el textarea a encriptar
+function leeAreaEncriptado(){//Función lee lo que hay en el textarea a encriptar
    if(inputText.value.length <=0){
     if(tamanio.clientWidth > 980){
         cajaEncriptado.style.background="url(imagenes/Muneco.png) no-repeat center 56px /80% #000000a6";
